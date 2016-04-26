72a6a1c19bd2dda86aa54819a00217b14283746c
Vincent Magnin <vincent.magnin@libertysurf.fr>
2016-04-26 19:11:33 +0200

    Ajout d'un fichier persoxmltv.py

--- pyxmltv.py
+++ pyxmltv.py
@@ -4,7 +4,7 @@
 '''
 Surveillance d'un fichier XMLTV contenant les programmes de la TNT pour les douze prochains jours. 
 ./pyxmltv.py -m mot1 mot2 mot3...
-Si aucun mot clé n'est fourni, la liste interne au script est utilisée.
+Si aucun mot clé n'est fourni, la liste interne au script est utilisée ou celle définie dans persoxmltv.py si ce fichier existe.
 Pour chercher une expression, la mettre entre guillemets.
 Exemple : ./pyxmltv.py -m "Linus Torvald" Stallman Linux
 La casse est prise en compte.
@@ -60,20 +60,21 @@ parsarg = argparse.ArgumentParser(description="Surveillance d'un fichier XMLTV c
 parsarg.add_argument("-m", action="store", nargs="+", help="Liste de mots-clés ou d'expressions (entre guillemets)", metavar="mot")
 parsarg.add_argument("-q", action="store_true", help="Pas d'affichage (quiet)")
 parsarg.add_argument("-p", action="store_true", help="Affichage uniquement en ligne de commandes (print)")
-parsarg.add_argument("-v", action="version", version="%(prog)s v0.93 Licence GPLv3", help="Version")
+parsarg.add_argument("-v", action="version", version="%(prog)s v0.94 Licence GPLv3", help="Version")
 args = parsarg.parse_args()
 
-# S'il n'y a pas de mots-clés en arguments, on utilise la liste (tuple) suivante par défaut :
-if len(sys.argv) == 1:
+# Si un fichier persoxmltv.py existe, on utilise ses définitions sinon on utilise ces listes par défaut :
+try:
+    from persoxmltv import *
+except:
     MOTS_CLES = ("Jude Law", "Star Wars", "La guerre des étoiles", "film d'animation", "téléfilm d'animation", "concert", "Led Zeppelin", "Arvo Pärt", "Bowie", "Björk", "écologie", "Anne Closset", "Yann Arthus-Bertrand", "nucléaire", "film de science-fiction", " astronomie", "chercheur", "brevets", "Snowden", "Linux", "Linus Torvald", "Stallman")
-else:
-    MOTS_CLES = args.m
+    TAGS_A_EXPLORER = ("title", "sub-title", "desc", "director", "actor", "composer", "date", "category")
+    CATEGORIES_A_EVITER = ("série", "série d'animation", "journal", "magazine sportif", "météo", "clips", "téléréalité")
+    CHAINE_RECUES = {'C1.telerama.fr': 'TF1', 'C2.telerama.fr': 'France 2', 'C3.telerama.fr': 'France 3', 'C112.telerama.fr': 'France 3 Nord Pas-de-Calais', 'C5.telerama.fr': 'France 5', 'C6.telerama.fr': 'M6', 'C7.telerama.fr': 'Arte', 'C8.telerama.fr': 'D8', 'C9.telerama.fr': 'W9', 'C10.telerama.fr': 'TMC', 'C11.telerama.fr': 'NT1', 'C12.telerama.fr': 'NRJ 12', 'C13.telerama.fr': 'France 4', 'C14.telerama.fr': 'La Chaîne Parlementaire', 'C15.telerama.fr': 'BFM TV', 'C16.telerama.fr': 'iTélé', 'C17.telerama.fr': 'D17', 'C18.telerama.fr': 'Gulli', 'C119.telerama.fr': 'France Ô', 'C4131.telerama.fr': 'HD1', 'C4132.telerama.fr': "L'Equipe 21", 'C4133.telerama.fr': '6ter', 'C4134.telerama.fr': 'Numéro 23', 'C4135.telerama.fr': 'RMC Découverte', 'C4136.telerama.fr': 'Chérie 25', 'C133.telerama.fr': 'LCI - La Chaîne Info', 'C87.telerama.fr': 'Euronews', 'C131.telerama.fr': 'La Une', 'C130.telerama.fr': 'La Deux', 'C811.telerama.fr': 'la Trois'}
 
-TAGS_A_EXPLORER = ("title", "sub-title", "desc", "director", "actor", "composer", "date", "category")
-
-CATEGORIES_A_EVITER = ("série", "série d'animation", "journal", "magazine sportif", "météo", "clips", "téléréalité")
-
-CHAINE_RECUES = {'C1.telerama.fr': 'TF1', 'C2.telerama.fr': 'France 2', 'C3.telerama.fr': 'France 3', 'C112.telerama.fr': 'France 3 Nord Pas-de-Calais', 'C5.telerama.fr': 'France 5', 'C6.telerama.fr': 'M6', 'C7.telerama.fr': 'Arte', 'C8.telerama.fr': 'D8', 'C9.telerama.fr': 'W9', 'C10.telerama.fr': 'TMC', 'C11.telerama.fr': 'NT1', 'C12.telerama.fr': 'NRJ 12', 'C13.telerama.fr': 'France 4', 'C14.telerama.fr': 'La Chaîne Parlementaire', 'C15.telerama.fr': 'BFM TV', 'C16.telerama.fr': 'iTélé', 'C17.telerama.fr': 'D17', 'C18.telerama.fr': 'Gulli', 'C119.telerama.fr': 'France Ô', 'C4131.telerama.fr': 'HD1', 'C4132.telerama.fr': "L'Equipe 21", 'C4133.telerama.fr': '6ter', 'C4134.telerama.fr': 'Numéro 23', 'C4135.telerama.fr': 'RMC Découverte', 'C4136.telerama.fr': 'Chérie 25', 'C133.telerama.fr': 'LCI - La Chaîne Info', 'C87.telerama.fr': 'Euronews', 'C131.telerama.fr': 'La Une', 'C130.telerama.fr': 'La Deux', 'C811.telerama.fr': 'la Trois'}
+# Mais s'il y a des mots-clés en arguments, c'est eux qu'on utilise :
+if args.m is not None:
+    MOTS_CLES = args.m    
 
 # Programmes des chaînes de la TNT gratuite, payante et des chaînes locales sur 12 jours :
 #telecharger_xmltv('http://kevinpato.free.fr/xmltv/download/tnt.zip')
@@ -143,10 +144,10 @@ for programme in RACINE.findall('programme'):
         if (texte[0] != "\n") & (texte != "None"):
             if element.tag == "title":
                 texte = "<h2>"+texte+"</h2>\n"
-                apres = " "
+                apres = ""
             elif element.tag == "sub-title":
                 texte = "<h3>"+texte+"</h3>\n"
-                apres = " "
+                apres = ""
             elif element.tag == "desc":
                 apres = "<br /> \n"
                 passages_a_la_ligne += 1
@@ -162,7 +163,7 @@ for programme in RACINE.findall('programme'):
             emission = emission + avant + texte + apres
 
     # On ajoute un séparateur (barre horizontale) entre chaque programme :
-    emission = "\n <hr /> \n" + emission
+    emission = "\n<hr /> \n" + emission
     # On met les mots-clés en gras :
     for mot in MOTS_CLES:
         emission = emission.replace(mot, "<strong>"+mot+"</strong>")
