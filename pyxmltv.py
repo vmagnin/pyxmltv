2a1e495b863e9e778985d844a0103418a7f8493b
Vincent Magnin <vincent.magnin@libertysurf.fr>
2016-05-07 14:45:11 +0200

    Pour les films, ajout d'un lien vers allocine.fr

--- pyxmltv.py
+++ pyxmltv.py
@@ -93,7 +93,7 @@ PARSARG.add_argument("-q", action="store_true",
 PARSARG.add_argument("-p", action="store_true",
                      help="Affichage uniquement en ligne de commandes (print)")
 PARSARG.add_argument("-v", action="version",
-                     version="%(prog)s v1.0 Licence GPLv3", help="Version")
+                     version="%(prog)s v1.1 Licence GPLv3", help="Version")
 ARGS = PARSARG.parse_args()
 
 # Si un fichier de configuration est spécifié, on l'utilise. Sinon si un fichier
